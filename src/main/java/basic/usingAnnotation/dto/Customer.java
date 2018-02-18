b2e28ce2ab6a3c2faae93573d1a323e0fc32ec13
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-02-18 15:26:44 +0530

    Added basic exception in spring.

---
+++ src/main/java/basic/usingAnnotation/dto/Customer.java
@@ -0,0 +1,23 @@
+package basic.usingAnnotation.dto;
+
+/**
+ * Created by pankaj on 10/28/2017.
+ */
+public class Customer {
+
+    private final long id;
+    private final String name;
+
+    public Customer(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
