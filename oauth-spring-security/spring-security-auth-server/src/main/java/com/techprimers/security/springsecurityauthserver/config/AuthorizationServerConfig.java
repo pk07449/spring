dc2269b468502dd6105abc4d99bb4abd5658b41f
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2019-05-10 00:51:21 +0530

    implemented OAuthSerever and OAuthResource server

--- oauth-spring-security/spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/config/AuthorizationServerConfig.java
+++ oauth-spring-security/spring-security-auth-server/src/main/java/com/techprimers/security/springsecurityauthserver/config/AuthorizationServerConfig.java
@@ -18,6 +18,58 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private AuthenticationManager authenticationManager;
 
     @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+            throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("my-trusted-client")
+                .authorizedGrantTypes("password","refresh_token", "implicit", "client_credentials", "authorization_code")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write", "trust")
+                .accessTokenValiditySeconds(60)
+                .redirectUris("http://localhost:8081/test.html")
+                .resourceIds("resource")
+                .secret("mysecret");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+            throws Exception {
+        oauthServer
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("permitAll()");
+    }
+}
+/*
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 
         security.tokenKeyAccess("permitAll()")
@@ -43,3 +95,75 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints.authenticationManager(authenticationManager);
     }
 }
+*/
+
+/*------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+package com.techprimers.security.springsecurityauthserver.config;
+
+
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.security.authentication.AuthenticationManager;
+        import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+        import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+        import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+        import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+        import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+        import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+        import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+        import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+        import org.springframework.security.oauth2.provider.token.TokenStore;
+        import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+        import javax.sql.DataSource;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+    @Bean
+    public DefaultAccessTokenConverter defaultAccessTokenConverter() {
+        return new DefaultAccessTokenConverter();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(this.tokenStore())
+                .authenticationManager(authenticationManager)
+                .accessTokenConverter(defaultAccessTokenConverter());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+            throws Exception {
+        oauthServer
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
+
+}
+*/
+/*------------------------------------------------------------------------------------------------------------------------*/
--- oauth-spring-security/spring-security-resource-server/src/main/java/com/techprimers/security/springsecurityauthserver/config/ResourceServerConfig.java
+++ oauth-spring-security/spring-security-resource-server/src/main/java/com/techprimers/security/springsecurityauthserver/config/ResourceServerConfig.java
@@ -1,46 +1,37 @@
+
 package com.techprimers.security.springsecurityauthserver.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
-
-@EnableResourceServer
-@EnableWebSecurity( debug = true )
 @Configuration
-public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.requestMatchers()
-                .antMatchers("/login", "/oauth/authorize")
-                .and()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .permitAll();
+    public void configure(ResourceServerSecurityConfigurer resources)
+            throws Exception {
+        resources.resourceId("resource");
     }
 
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers("/foo", "/bar", "/hello", "/test").authenticated().
+                and().csrf().disable();
+    }
 
-        auth.parentAuthenticationManager(authenticationManager)
-                .inMemoryAuthentication()
-                .withUser("Peter")
-                .password("peter")
-                .roles("USER");
+    @Bean
+    public RemoteTokenServices LocalTokenService() {
+        final RemoteTokenServices tokenService = new RemoteTokenServices();
+        tokenService.setCheckTokenEndpointUrl("http://localhost:8081/oauth/check_token");
+        tokenService.setClientId("my-trusted-client");
+        tokenService.setClientSecret("mysecret");
+        return tokenService;
     }
 }
