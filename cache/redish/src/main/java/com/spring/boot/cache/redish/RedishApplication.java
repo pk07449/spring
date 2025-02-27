a8d123dd7232bea6ba8f7ec4311d13aae28a3434
Panakj Kumar <java.j2ee.pankaj@gamil.com>
2025-02-27 18:00:53 +0530

    initial commit for redish cache

---
+++ cache/redish/src/main/java/com/spring/boot/cache/redish/RedishApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.cache.redish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedishApplication.class, args);
+	}
+
+}
