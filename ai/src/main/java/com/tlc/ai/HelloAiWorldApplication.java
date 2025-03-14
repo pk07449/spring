63920177447d0bb558f5dc9835f15f981e5eb0ab
Panakj Kumar <java.j2ee.pankaj@gamil.com>
2025-03-15 03:17:56 +0530

    Added my first AI hello-ai-world

---
+++ ai/src/main/java/com/tlc/ai/HelloAiWorldApplication.java
@@ -0,0 +1,25 @@
+package com.tlc.ai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HelloAiWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAiWorldApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(ChatClient.Builder builder) {
+		return args -> {
+			ChatClient chatClient = builder.build();
+			String response = chatClient.prompt("Who is the PM of india").call().content();
+			System.out.println(response);
+		};
+	}
+
+}
