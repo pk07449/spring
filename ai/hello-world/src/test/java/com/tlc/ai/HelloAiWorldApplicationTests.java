28b558dbbc60d4978f6224190071a7f5e26a44c1
Panakj Kumar <java.j2ee.pankaj@gamil.com>
2025-03-17 09:42:49 +0530

    Added ai chat-client and custome response

---
+++ ai/hello-world/src/test/java/com/tlc/ai/HelloAiWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlc.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAiWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
