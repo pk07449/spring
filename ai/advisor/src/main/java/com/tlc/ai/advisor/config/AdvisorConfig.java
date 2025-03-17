49ac8462a48789ec682c4d4edb2c7f155c1a5cdf
Panakj Kumar <java.j2ee.pankaj@gamil.com>
2025-03-18 00:24:56 +0530

    Added advisor and rag hello world

--- ai/advisor/src/main/java/com/tlc/ai/advisor/config/AdvisorConfig.java
+++ ai/advisor/src/main/java/com/tlc/ai/advisor/config/AdvisorConfig.java
@@ -1,6 +1,15 @@
 package com.tlc.ai.advisor.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +17,33 @@ import org.springframework.context.annotation.Configuration;
 public class AdvisorConfig {
 
     @Bean
-    public ChatClient chatClient (ChatClient.Builder chatClientBuilder) {
-        return chatClientBuilder.build();
+     public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
+    @Bean
+    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+    @Bean
+    public ChatClient chatClient (ChatClient.Builder builder, ChatModel chatModel, ChatMemory chatMemory, VectorStore vectorStore) {
+        return builder
+                .defaultSystem("""
+                    You are a customer chat support agent of an airline named "Funnair". Respond in a friendly,
+                    helpful, and joyful manner.
+
+                    Before providing information about a booking or cancelling a booking, you MUST always
+                    get the following information from the user: booking number, customer first name and last name.
+
+                    Before changing a booking you MUST ensure it is permitted by the terms.
+
+                    If there is a charge for the change, you MUST ask the user to consent before proceeding.
+                    """)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory), // CHAT MEMORY
+                        new QuestionAnswerAdvisor(vectorStore), // RAG
+                        new SimpleLoggerAdvisor())
+                .defaultFunctions("getBookingDetails", "changeBooking", "cancelBooking") // FUNCTION CALLING
+                .build();
     }
 }
---
+++ ai/advisor/src/main/java/com/tlc/ai/advisor/resource/RAGController.java
@@ -0,0 +1,80 @@
+package com.tlc.ai.advisor.resource;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+class RAGController {
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    RAGController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
+        this.chatClient = chatClientBuilder.build();
+        this.vectorStore = vectorStore;
+    }
+    
+    // Assume that we have already read the documents from files containing information about people 
+    // and stored them in the VectorStore as described in the previous section.
+    
+    @GetMapping("/ai/rag/people")
+    Person chatWithRag(@RequestParam String name) {
+        // Querying the VectorStore using natural language looking for the information about a person.
+        List<Document> similarDocuments =
+                vectorStore.similaritySearch(SearchRequest.builder().query(name).topK(2).build());
+        String information = similarDocuments.stream()
+                .map(Document::getFormattedContent)
+                .collect(Collectors.joining(System.lineSeparator()));
+        
+        // Constructing the systemMessage to indicate the AI model to use the passed information
+        // to answer the question.
+        var systemPromptTemplate = new SystemPromptTemplate("""
+              You are a helpful assistant.
+              
+              Use the following information to answer the question:
+              {information}
+              """);
+        var systemMessage = systemPromptTemplate.createMessage(
+                Map.of("information", information));
+
+        // Using BeanOutputConverter to parse the response into an instance of Person.
+        var outputConverter = new BeanOutputConverter<>(Person.class);
+        
+        // Constructing the userMessage to ask the AI model to tell about the person.
+        PromptTemplate userMessagePromptTemplate = new PromptTemplate("""
+        Tell me about {name} as if current date is {current_date}.
+
+        {format}
+        """);
+        Map<String,Object> model = Map.of("name", name,
+                "current_date", LocalDate.now(),
+                "format", outputConverter.getFormat());
+        var userMessage = new UserMessage(userMessagePromptTemplate.create(model).getContents());
+
+        var prompt = new Prompt(List.of(systemMessage, userMessage));
+
+        var response = chatClient.prompt(prompt).call().content();
+
+        return outputConverter.convert(response);
+    }
+}
+
+record Person(String name,
+              String dateOfBirth,
+              int experienceInYears,
+              List<String> books) {
+}
